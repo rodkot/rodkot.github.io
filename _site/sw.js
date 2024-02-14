eac9694d330c7bdb152a490f52fa58526263829f
Rodion Kotov <r.kotov@rodkot.site>
2024-02-14 16:04:37 +0700

    Добавил: - информацию о ШИФТ Лаб

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240110.143805';
+const cacheName = 'chirpy-20240214.170330';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
